d4aa1258cc29c3a3577edd8a030f421fa5e8e64e
Alexey Popov <alexey.popov@motorsport.com>
2021-07-07 14:17:01 +0300

    Updated companies

--- template-parts/companies.php
+++ template-parts/companies.php
@@ -2,11 +2,12 @@
 	<div class="container">
 		<h2 class="heading dots">О нас пишут</h2>
 		<div class="companies">
-			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="logo">
-			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="logo">
-			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="logo">
-			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="logo">
-			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="logo">
+			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-yorker-logo.png" alt="the-new-yorker">
+			<img src="<?php echo get_template_directory_uri() ?>/images/the-verge-logo.png" alt="the-verge">
+			<img src="<?php echo get_template_directory_uri() ?>/images/the-new-york-times-logo.png"
+			     alt="the-new-york-times">
+			<img src="<?php echo get_template_directory_uri() ?>/images/vanity-fair-logo.png" alt="vanity-fair">
+			<img src="<?php echo get_template_directory_uri() ?>/images/fast-company-logo.png" alt="fast-company">
 		</div>
 	</div>
 </section>
