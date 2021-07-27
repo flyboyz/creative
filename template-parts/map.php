0f36e9c7cf6b3e9516ce34106aa444e4435835d3
Alexey Popov <alexey.popov@motorsport.com>
2021-07-27 13:15:02 +0300

    Disable map scrollZoom

--- template-parts/map.php
+++ template-parts/map.php
@@ -6,6 +6,6 @@ $zoom    = get_field( 'coord' ) ? 17 : 15;
 <section class="p-0">
 	<h2 class="heading dots"><?php echo $heading ?></h2>
 	<div class="map">
-		<?php echo do_shortcode( "[yamap center='$coord' zoom='$zoom' height='100%' type='yandex#map' controls='typeSelector;zoomControl' mobiledrag='0'][yaplacemark coord='$coord' icon='islands#dotIcon' color='#de547b' name='Placemark'][/yamap]" ) ?>
+		<?php echo do_shortcode( "[yamap center='$coord' zoom='$zoom' height='100%' type='yandex#map' controls='typeSelector;zoomControl' scrollzoom='0' mobiledrag='0'][yaplacemark coord='$coord' icon='islands#dotIcon' color='#de547b' name='Placemark'][/yamap]" ) ?>
 	</div>
 </section>
