f61f5ef5af29fdeb8ea7011f400c2ca59b8c5b25
Alexey Popov <alexey.popov@motorsport.com>
2021-07-27 11:46:50 +0300

    Design update

--- template-parts/map.php
+++ template-parts/map.php
@@ -1,7 +1,11 @@
 <?php
 $heading = is_single() ? 'Где находится ' . get_the_title() . '?' : 'Где мы находимся?';
+$coord   = get_field( 'coord' ) ?? '45.1884, 33.3592';
+$zoom    = get_field( 'coord' ) ? 17 : 15;
 ?>
 <section class="p-0">
 	<h2 class="heading dots"><?php echo $heading ?></h2>
-	<img src="<?= get_template_directory_uri() ?>/images/map.png" alt="map" class="block">
+	<div class="map">
+		<?php echo do_shortcode( "[yamap center='$coord' zoom='$zoom' height='100%' type='yandex#map' controls='typeSelector;zoomControl' mobiledrag='0'][yaplacemark coord='$coord' icon='islands#dotIcon' color='#de547b' name='Placemark'][/yamap]" ) ?>
+	</div>
 </section>
--- template-parts/ticket-main.php
+++ template-parts/ticket-main.php
@@ -1,37 +1,43 @@
-<section class="container my-12 text-center">
-	<div class="ticket">
-		<div class="left-side">
-			<p>Единый<br>билет</p>
-		</div>
-		<div class="ticket__content">
-			<p class="title">Выгодное предложение</p>
-			<div class="flex p-4 items-center">
-				<img src="<?php
-				echo get_template_directory_uri() ?>/images/logo.png"
-				     class="ticket__logo" alt="ticket-logo">
-				<ul>
-					<li>Музей цвета и сладостей</li>
-					<li>Зомби-автобус</li>
-					<li>Труба "Вортекс"</li>
-					<li>Перевернутый дом</li>
-					<li>Музей инопланетян</li>
-					<li>Золотоискатели</li>
-                    <li>Дом великана</li>
-                </ul>
+<section>
+	<div class="container text-center">
+		<div class="ticket">
+			<div class="left-side">
+				<p>Единый<br>билет</p>
 			</div>
-			<div class="ticket__calculator">
-				<div class="count-input">
-					<div class="minus cursor-pointer">-</div>
-					<div class="count">1</div>
-					<div class="plus cursor-pointer">+</div>
+			<div class="ticket__content">
+				<p class="title">Выгодное предложение</p>
+				<div class="flex p-4 flex-col md:flex-row items-center">
+					<img src="<?php
+					echo get_template_directory_uri() ?>/images/logo.png"
+					     class="ticket__logo" alt="ticket-logo">
+					<ul>
+						<li>Музей цвета и сладостей</li>
+						<li>Зомби-автобус</li>
+						<li>Труба "Вортекс"</li>
+						<li>Перевернутый дом</li>
+						<li>Музей инопланетян</li>
+						<li>Золотоискатели</li>
+						<li>Дом великана</li>
+					</ul>
 				</div>
-				<div>&times;</div>
-				<div class="relative">
-					<div class="price-old"><span>1350</span> &#8381;</div>
-					<div class="price"><span>1200</span> &#8381;</div>
+				<div class="ticket__calculator">
+					<div>
+						<div class="count-input">
+							<div class="minus cursor-pointer">-</div>
+							<span class="count">1</span>
+							<div class="plus cursor-pointer">+</div>
+						</div>
+						<span>&times;</span>
+						<div class="relative">
+							<span class="price-old"><span>1350</span>&#8381;</span>
+							<span class="price"><span>1200</span>&#8381;</span>
+						</div>
+					</div>
+					<div>
+						<span>=</span>
+						<span class="sum"><span>1200</span>&#8381;</span>
+					</div>
 				</div>
-				<div>=</div>
-				<div class="sum"><span>1200</span> &#8381;</div>
 			</div>
 		</div>
 	</div>
